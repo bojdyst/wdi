327faacb9a8923756b04d3ff35992b8f6e1821a1
unknown <tbojdys@student.agh.edu.pl>
2021-10-28 20:19:44 +0200

    6 added

--- laboratorium_3/zadanie_6.py
+++ laboratorium_3/zadanie_6.py
@@ -1,7 +1,29 @@
 a = int(input("Wprowadź pierwszą liczbę "))
 b = int(input("Wprowadź drugą liczbę "))
 
-print(a+b) 
-print(a-b) 
-print(a*b) 
-print(a/b) 
+def function_multiplication():
+    print("Wynik dodawania", a+b) 
+    print("Wynik odejmowania",a-b) 
+    print("Wynik mnożenia", a*b)    
+    print("Wynik dzielenia", a/b) 
+    print("Wynik kwadratowania", "Pierwsza liczba", a**2, "Druga liczba", b**2)
+    print("Wynik pierwiastkowania", "Pierwsza liczba", a**(1/2), "Druga liczba", b**(1/2))
+
+def function_print():
+    if a*b==10:
+        function_multiplication()
+        print("Wynik mnożenia to 10, Yay!")
+    else: 
+        function_multiplication()
+
+if a<0 and b<0:
+    print("Obie liczby są mniejsze od 0 - program zostanie zakończony")
+    pass 
+elif (a>0) and (b<0):
+    b = b * (-1)
+    function_print()
+elif (a<0) and (b>0):
+    a = a * (-1)
+    function_print()
+else:
+    function_print()
