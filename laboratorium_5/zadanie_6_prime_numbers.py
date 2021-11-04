299e0c8ae0e44164389c8fd2994a54aa50f47a09
unknown <tbojdys@student.agh.edu.pl>
2021-11-04 15:58:30 +0100

    task 6 added

---
+++ laboratorium_5/zadanie_6_prime_numbers.py
@@ -0,0 +1,23 @@
+#Sprawdź, czy zadana liczba jest liczbą pierwszą
+
+def czy_jest_pierwszą():
+    liczba = int(input("Wprowadź liczbę i sprawdź, czy jest liczbą pierwszą!: "))
+    if liczba < 2:
+        print("Liczba", liczba, "NIE jest liczbą pierwszą!")
+    elif liczba == 2:
+        print("Liczba", liczba, "jest liczbą pierwszą!")
+    elif liczba > 2:
+        for i in range(2, liczba):
+            if liczba % i == 0:
+                print("Liczba", liczba, "NIE jest liczbą pierwszą!")
+                return
+        print("Liczba", liczba, "jest liczbą pierwszą!")
+
+while True: 
+    czy_jest_pierwszą()
+    loop = input("Czy chesz wprowadzić kolejną liczbę? Wpisz T/N: ")
+    if loop == "N":
+        print("Dziękuję za skorzystanie z programu! Miłego dnia ;)")
+        break  
+
+
