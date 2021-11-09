69c9e5ef358ae59596ad36e9010e8f1a33998670
unknown <tbojdys@student.agh.edu.pl>
2021-11-09 19:54:30 +0100

    The base of task 2

---
+++ laboratorium_6/zadanie_2_complex_numbers.py
@@ -0,0 +1,21 @@
+import cmath
+
+#Wykonaj działania na liczbach zespolonych już dziś!
+
+z1 = complex(input("Wprowadź pierwszą liczbę zespoloną (w postaci a+bj): "))
+z2 = complex(input("Wprowadź pierwszą liczbę zespoloną (w postaci: a+bj): " ))
+
+#dodawanie
+sum = print("Wynik dodawania =", z1 + z2)
+
+#odejmowanie
+difference = print("Wynik odejmowania =", z1 - z2)
+
+#mnożenie
+multiplication = print("Wynik mnożenia =", z1 * z2)
+
+#dzielenie 
+division = print("Wynik dzielenia =", z1 / z2)
+
+#potęgowanie
+exponentiation = print("Wynik potęgowania =", z1 ** z2)
