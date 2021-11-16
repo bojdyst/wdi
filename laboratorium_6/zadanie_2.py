3b305726f6d25dcbccc4db03e867b74cfac747ff
unknown <tbojdys@student.agh.edu.pl>
2021-11-16 13:25:34 +0100

    Try and except added

--- laboratorium_6/zadanie_2.py
+++ laboratorium_6/zadanie_2.py
@@ -1,26 +1,34 @@
 #Kalkulator liczb zespolonych
 
 def kalkulator():
-    z1 = complex(input("Wprowadź pierwszą liczbę zespoloną (w postaci a+bj): "))
-    operator = input("Wprowadź operator (+, -, *, /, **): ")
-    if operator != "+" and operator != "-" and operator != "*" and operator != "/" and operator != "**":
-        print("Nieznany operator!")
-        return
-    z2 = complex(input("Wprowadź drugą liczbę zespoloną (w postaci: a+bj): " ))
-    if operator == "+":
-        print("Wynik dodawania =", z1 + z2)
-    elif operator == "-":
-        print("Wynik odejmowania =", z1 - z2)
-    elif operator == "*":
-        print("Wynik mnożenia =", z1 * z2)
-    elif operator == "/":
-        print("Wynik dzielenia =", z1 / z2)
-    elif operator == "**":
-        if z2.imag == 0:
-            print("Wynik potęgowania =", z1 ** z2)
-        else:
-            raise Exception("Wystąpił błąd podczas potęgowania - nieobsługiwana operacja!")
-        
+    try:
+        z1 = complex(input("Wprowadź pierwszą liczbę zespoloną (w postaci a+bj): "))
+        operator = input("Wprowadź operator (+, -, *, /, **): ")
+        if operator != "+" and operator != "-" and operator != "*" and operator != "/" and operator != "**":
+            print("Nieznany operator!")
+            return
+        z2 = complex(input("Wprowadź drugą liczbę zespoloną (w postaci: a+bj): " ))
+        if operator == "+":
+            print("Wynik dodawania =", z1 + z2)
+        elif operator == "-":
+            print("Wynik odejmowania =", z1 - z2)
+        elif operator == "*":
+            print("Wynik mnożenia =", z1 * z2)
+        elif operator == "/":
+            if z2 == 0:
+                raise ZeroDivisionError
+            else:
+                print("Wynik dzielenia =", z1 / z2)
+        elif operator == "**":
+            if z2.imag == 0:
+                print("Wynik potęgowania =", z1 ** z2)
+            else:
+                raise ValueError
+    except ValueError:
+        print("Wystąpił błąd podczas potęgowania - nieobsługiwana operacja!") 
+    except ZeroDivisionError:
+        print("Nie dziel przez zero!") 
+
 while True:
     kalkulator()
     loop = input("Czy chcesz skorzystać z kalkulatora jeszcze raz? T/N: ")
--- laboratorium_6/zadanie_2_complex_numbers.py
+++ laboratorium_6/zadanie_2_complex_numbers.py
@@ -12,10 +12,19 @@ difference = print("Wynik odejmowania =", z1 - z2)
 multiplication = print("Wynik mnożenia =", z1 * z2)
 
 #dzielenie 
-division = print("Wynik dzielenia =", z1 / z2)
+try:
+    if z2 == 0:
+        raise ZeroDivisionError
+    else:
+        division = print("Wynik dzielenia =", z1 / z2)
+except ZeroDivisionError:
+    print("Nie dziel przez zero!")    
 
 #potęgowanie
-if z2.imag == 0:
-    exponentiation = print("Wynik potęgowania =", z1 ** z2)
-else:
-    raise Exception("Wystąpił błąd podczas potęgowania - nieobsługiwana operacja!")
+try:
+    if z2.imag == 0:
+        exponentiation = print("Wynik potęgowania =", z1 ** z2)
+    else:
+        raise ValueError("Wystąpił błąd podczas potęgowania - nieobsługiwana operacja!")
+except ValueError:
+    print("Wystąpił błąd podczas potęgowania - nieobsługiwana operacja!") 
